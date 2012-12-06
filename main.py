4a24672357ae8692510e3972d2d4b6be93cf09dc
Raitis Stengrevics <dagrevis@gmail.com>
2012-12-07 00:05:24 +0200

    "Hello, world!" w/ templates approach.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+
+
+app = flask.Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "Hello, world!"
+    return flask.render_template("hello_world.html")
 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
