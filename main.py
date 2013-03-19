6ba9765e858f03f2f5c554fe98a598a0abe2244d
Jānis Ābele <janis.abele@gmail.com>
2013-03-19 22:32:58 +0200

    Show next meetup information on landing page

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-
 import flask
+
+from meetup import PYLUGMeetup
 
 
 app = flask.Flask(__name__)
@@ -6,9 +9,19 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    return flask.render_template("landing.html")
+    event = meetup.next_event()
+    return flask.render_template("landing.html", event=event)
+
+
+def _parse_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--meetup-key')
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
+    args = _parse_args()
+    meetup = PYLUGMeetup(args.meetup_key or '3b6a2c23416b587c54452c2f75263830')
     app.debug = True
     app.run()
